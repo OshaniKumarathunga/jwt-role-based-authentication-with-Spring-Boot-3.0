086b04a2deecc9739d48077c14c3734cd9065fc0
Oshani Kumarathunga <oshanipbkonline@gmail.com>
2023-06-28 23:18:45 +0530

    spring Boot project Created

---
+++ jwtauthentication/src/test/java/com/springboot3/jwtauthentication/JwtauthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot3.jwtauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtauthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
