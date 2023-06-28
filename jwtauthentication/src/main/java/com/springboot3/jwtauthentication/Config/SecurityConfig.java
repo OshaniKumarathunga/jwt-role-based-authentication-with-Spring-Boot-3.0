8806aa00dcaa685fa7f68e67be7ac5c773a7c6ac
Oshani Kumarathunga <oshanipbkonline@gmail.com>
2023-06-28 23:58:46 +0530

    Configuration added

---
+++ jwtauthentication/src/main/java/com/springboot3/jwtauthentication/Config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.springboot3.jwtauthentication.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf(csrf ->csrf.disable())
+                .authorizeHttpRequests(requests -> requests.requestMatchers("/authernticate").permitAll()
+                        .requestMatchers(HttpHeaders.ALLOW).permitAll()
+                .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+        ;
+
+
+        return httpSecurity.build();
+    }
+}
